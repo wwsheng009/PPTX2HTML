e28d471a079ad73cb4dbfb5c32a6e4e1252249cb
wwsheng009 <vincentwwsheng@gmail.com>
2023-11-23 16:48:49 +0800

    update

--- js/pptx2html.js
+++ js/pptx2html.js
@@ -1,33 +1,118 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+	function getURLParameter(parameterName) {
+		const urlParams = new URLSearchParams(window.location.search);
+		return urlParams.get(parameterName);
+	}
 
 	if (window.Worker) {
-		
+		const fname = getURLParameter('file');
+		if (!fname) {
+			alert('Failed to get the pptx file')
+			return
+		}
 		var $result = $("#result");
 		var isDone = false;
-		
-		$("#uploadBtn").on("change", function(evt) {
-			
+
+		$result.html("");
+		$("#load-progress").text("0%").attr("aria-valuenow", 0).css("width", "0%");
+		$("#result_block").removeClass("hidden").addClass("show");
+		// Fetch the file from the server
+		fetch(fname)
+			.then(response => response.arrayBuffer())
+			.then(data => {
+				// Web Worker
+				var worker = new Worker('./js/worker.js');
+
+				worker.addEventListener('message', function (e) {
+					var msg = e.data;
+
+					switch (msg.type) {
+						case "progress-update":
+							$("#load-progress").text(msg.data.toFixed(2) + "%")
+								.attr("aria-valuenow", msg.data.toFixed(2))
+								.css("width", msg.data.toFixed(2) + "%");
+							if (msg.data == 100) {
+								$("#load-progress-row").hide()
+							}
+							break;
+						case "slide":
+							$result.append(msg.data);
+							break;
+						case "processMsgQueue":
+							processMsgQueue(msg.data);
+							break;
+						case "pptx-thumb":
+							$("#pptx-thumb").attr("src", "data:image/jpeg;base64," + msg.data);
+							break;
+						case "slideSize":
+							if (localStorage) {
+								localStorage.setItem("slideWidth", msg.data.width);
+								localStorage.setItem("slideHeight", msg.data.height);
+							} else {
+								alert("Browser don't support Web Storage!");
+							}
+							break;
+						case "globalCSS":
+							$result.append("<style>" + msg.data + "</style>");
+							break;
+						case "ExecutionTime":
+							$("#info_block").html("Execution Time: " + msg.data + " (ms)");
+							isDone = true;
+							worker.postMessage({
+								"type": "getMsgQueue"
+							});
+							break;
+						case "WARN":
+							console.warn('Worker: ', msg.data);
+							break;
+						case "ERROR":
+							console.error('Worker: ', msg.data);
+							$("#error_block").text(msg.data);
+							break;
+						case "DEBUG":
+							console.debug('Worker: ', msg.data);
+							break;
+						case "INFO":
+						default:
+							console.info('Worker: ', msg.data);
+						//$("#info_block").html($("#info_block").html() + "<br><br>" + msg.data);
+					}
+				}, false);
+
+				worker.postMessage({
+					"type": "processPPTX",
+					"data": data
+				});
+			})
+			.catch(error => {
+				console.error('Error fetching the file:', error);
+				$("#error_block").text("Error fetching the file");
+			});
+
+		$("#uploadBtn").on("change", function (evt) {
+
 			isDone = false;
-			
+
 			$result.html("");
 			$("#load-progress").text("0%").attr("aria-valuenow", 0).css("width", "0%");
 			$("#result_block").removeClass("hidden").addClass("show");
-			
+
 			var fileName = evt.target.files[0];
-			
+
 			// Read the file
 			var reader = new FileReader();
-			reader.onload = (function(theFile) {
-				return function(e) {
-					
+			reader.onload = (function (theFile) {
+				return function (e) {
+
 					// Web Worker
 					var worker = new Worker('./js/worker.js');
-				
-					worker.addEventListener('message', function(e) {
-						
+
+					worker.addEventListener('message', function (e) {
+
 						var msg = e.data;
-						
-						switch(msg.type) {
+
+						switch (msg.type) {
 							case "progress-update":
 								$("#load-progress").text(msg.data.toFixed(2) + "%")
 									.attr("aria-valuenow", msg.data.toFixed(2))
@@ -73,27 +158,27 @@ $(document).ready(function() {
 							case "INFO":
 							default:
 								console.info('Worker: ', msg.data);
-								//$("#info_block").html($("#info_block").html() + "<br><br>" + msg.data);
+							//$("#info_block").html($("#info_block").html() + "<br><br>" + msg.data);
 						}
-						
+
 					}, false);
-					
+
 					worker.postMessage({
 						"type": "processPPTX",
 						"data": e.target.result
 					});
-					
+
 				}
 			})(fileName);
 			reader.readAsArrayBuffer(fileName);
-			
+
 		});
-		
+
 		$("#slideContentModel").on("show.bs.modal", function (e) {
 			if (!isDone) { return; }
 			$("#slideContentModel .modal-body textarea").text($result.html());
 		});
-		
+
 		$("#download-btn").click(function () {
 			if (!isDone) { return; }
 			var cssText = "";
@@ -103,19 +188,19 @@ $(document).ready(function() {
 				var headHtml = "<style>" + cssText + "</style>";
 				var bodyHtml = $result.html();
 				var html = "<!DOCTYPE html><html><head>" + headHtml + "</head><body>" + bodyHtml + "</body></html>";
-				var blob = new Blob([html], {type: "text/html;charset=utf-8"});
+				var blob = new Blob([html], { type: "text/html;charset=utf-8" });
 				saveAs(blob, "slides_p.html");
 			});
 		});
-		
+
 		$("#download-reveal-btn").click(function () {
 			if (!isDone) { return; }
 			var cssText = "";
 			$.get("css/pptx2html.css", function (data) {
 				cssText = data;
 			}).done(function () {
-				var revealPrefix = 
-"<script type='text/javascript'>\
+				var revealPrefix =
+					"<script type='text/javascript'>\
 Reveal.initialize({\
 	controls: true,\
 	progress: true,\
@@ -140,54 +225,54 @@ Reveal.initialize({\
 				var headHtml = "<style>" + cssText + "</style>";
 				var bodyHtml = "<div id='slides' class='slides'>" + $result.html() + "</div>";
 				var html = revealPrefix + headHtml + bodyHtml;
-				var blob = new Blob([html], {type: "text/html;charset=utf-8"});
+				var blob = new Blob([html], { type: "text/html;charset=utf-8" });
 				saveAs(blob, "slides.html");
 			});
 		});
-		
+
 		$("#to-reveal-btn").click(function () {
 			if (localStorage) {
 				localStorage.setItem("slides", LZString.compressToUTF16($result.html()));
-				window.open("./reveal/demo.html", "_blank");
+				window.open("./reveal/play.html", "_blank");
 			} else {
 				alert("Browser don't support Web Storage!");
 			}
 		});
-		
+
 	} else {
-		
+
 		alert("Browser don't support Web Worker!");
-		
+
 	}
-	
+
 });
 
 function processMsgQueue(queue) {
-	for (var i=0; i<queue.length; i++) {
+	for (var i = 0; i < queue.length; i++) {
 		processSingleMsg(queue[i].data);
 	}
 }
 
 function processSingleMsg(d) {
-	
+
 	var chartID = d.chartID;
 	var chartType = d.chartType;
 	var chartData = d.chartData;
 
-	var data =  [];
-	
+	var data = [];
+
 	var chart = null;
 	switch (chartType) {
 		case "lineChart":
 			data = chartData;
 			chart = nv.models.lineChart()
-						.useInteractiveGuideline(true);
-			chart.xAxis.tickFormat(function(d) { return chartData[0].xlabels[d] || d; });
+				.useInteractiveGuideline(true);
+			chart.xAxis.tickFormat(function (d) { return chartData[0].xlabels[d] || d; });
 			break;
 		case "barChart":
 			data = chartData;
 			chart = nv.models.multiBarChart();
-			chart.xAxis.tickFormat(function(d) { return chartData[0].xlabels[d] || d; });
+			chart.xAxis.tickFormat(function (d) { return chartData[0].xlabels[d] || d; });
 			break;
 		case "pieChart":
 		case "pie3DChart":
@@ -197,41 +282,41 @@ function processSingleMsg(d) {
 		case "areaChart":
 			data = chartData;
 			chart = nv.models.stackedAreaChart()
-						.clipEdge(true)
-						.useInteractiveGuideline(true);
-			chart.xAxis.tickFormat(function(d) { return chartData[0].xlabels[d] || d; });
+				.clipEdge(true)
+				.useInteractiveGuideline(true);
+			chart.xAxis.tickFormat(function (d) { return chartData[0].xlabels[d] || d; });
 			break;
 		case "scatterChart":
-			
-			for (var i=0; i<chartData.length; i++) {
+
+			for (var i = 0; i < chartData.length; i++) {
 				var arr = [];
-				for (var j=0; j<chartData[i].length; j++) {
-					arr.push({x: j, y: chartData[i][j]});
+				for (var j = 0; j < chartData[i].length; j++) {
+					arr.push({ x: j, y: chartData[i][j] });
 				}
-				data.push({key: 'data' + (i + 1), values: arr});
+				data.push({ key: 'data' + (i + 1), values: arr });
 			}
-			
+
 			//data = chartData;
 			chart = nv.models.scatterChart()
-						.showDistX(true)
-						.showDistY(true)
-						.color(d3.scale.category10().range());
+				.showDistX(true)
+				.showDistY(true)
+				.color(d3.scale.category10().range());
 			chart.xAxis.axisLabel('X').tickFormat(d3.format('.02f'));
 			chart.yAxis.axisLabel('Y').tickFormat(d3.format('.02f'));
 			break;
 		default:
 	}
-	
+
 	if (chart !== null) {
-		
+
 		d3.select("#" + chartID)
 			.append("svg")
 			.datum(data)
 			.transition().duration(500)
 			.call(chart);
-		
+
 		nv.utils.windowResize(chart.update);
-		
+
 	}
-	
+
 }
